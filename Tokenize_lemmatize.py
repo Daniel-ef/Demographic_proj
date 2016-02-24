df46fbd14b104a18d46fdc5043932c80b33d5066
Daniil_ef <dan.997@mail.ru>
2016-02-24 15:30:51 +0300

    Разбивка текста на токены, его лемматизация. Исключение стоп-слов.

---
+++ Tokenize_lemmatize.py
@@ -0,0 +1,12 @@
+import re
+import pymorphy2
+morph = pymorphy2.MorphAnalyzer()
+
+with open ('db/stop_word_dic', 'r') as f:
+    stop_words = f.read().split('\n')
+with open('db/text_for_test', 'r') as f:
+    dic_norm_words = [morph.parse(word)[0].normal_form for word in re.findall('[а-яА-Я]+[-\']?[а-яА-Я]*', f.read())]
+
+for el in dic_norm_words:
+    if el not in stop_words:
+        print(el, end='\n')
