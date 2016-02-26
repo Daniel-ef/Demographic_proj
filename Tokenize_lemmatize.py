c4e81e69003dc8097df67e34a53ce42b828ba975
Daniil_ef <dan.997@mail.ru>
2016-02-26 16:23:56 +0300

    Добавлено удаление обращений в начале строки, ссылок, html-тегов.

--- Tokenize_lemmatize.py
+++ Tokenize_lemmatize.py
@@ -4,8 +4,14 @@ morph = pymorphy2.MorphAnalyzer()
 
 with open ('db/stop_word_dic', 'r') as f:
     stop_words = f.read().split('\n')
+
 with open('db/text_for_test', 'r') as f:
-    dic_norm_words = [morph.parse(word)[0].normal_form for word in re.findall('[а-яА-Я]+-?[а-яА-Я]*', f.read())]
+    text = f.read()
+text = text[re.match('\w+, ?', text).end():]  # Удаляет обращения в начале строки
+text = re.sub('\S+\.\w+', '', text)  # Удаляет ссылки
+text = re.sub('<[^>]+>', '', text)  #  Удаляет html-теги
+
+dic_norm_words = [morph.parse(word)[0].normal_form for word in re.findall('[a-zA-Zа-яА-Я]+-?[a-zA-Zа-яА-Я]*', text)]
 
 for el in dic_norm_words:
     if el not in stop_words:
