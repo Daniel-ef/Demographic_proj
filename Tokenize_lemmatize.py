248a511add25e09e45abe99b4fe4a2c6282a5b31
Daniil_ef <dan.997@mail.ru>
2016-03-19 20:08:42 +0300

    Реализует функцию лемматизации и нормализации слов. Удаляет обращения, ссылки, html-теги.

--- Tokenize_lemmatize.py
+++ Tokenize_lemmatize.py
@@ -1,18 +1,21 @@
 import re
 import pymorphy2
-morph = pymorphy2.MorphAnalyzer()
 
-with open ('db/stop_word_dic', 'r') as f:
-    stop_words = f.read().split('\n')
 
-with open('db/text_for_test', 'r') as f:
-    text = f.read()
-text = text[re.match('\w+, ?', text).end():]  # Удаляет обращения в начале строки
-text = re.sub('\S+\.\w+', '', text)  # Удаляет ссылки
-text = re.sub('<[^>]+>', '', text)  #  Удаляет html-теги
+def lemma_text(text):
+    morph = pymorphy2.MorphAnalyzer()
+    with open ('db/stop_word_dic', 'r') as f:
+        stop_words = f.read().split('\n')
 
-dic_norm_words = [morph.parse(word)[0].normal_form for word in re.findall('[a-zA-Zа-яА-Я]+-?[a-zA-Zа-яА-Я]*', text)]
+    if (re.match('\w+, ?', text)):
+        text = text[re.match('\w+, ?', text).end():]  # Удаляет обращения в начале строки
+    text = re.sub('\S+\.\w+', '', text)  # Удаляет ссылки
+    text = re.sub('<[^>]+>', '', text)  #  Удаляет html-теги
 
-for el in dic_norm_words:
-    if el not in stop_words:
-        print(el, end='\n')
+    vec_norm_words = [morph.parse(word)[0].normal_form for word in re.findall('[a-zA-Zа-яА-Я]+-?[a-zA-Zа-яА-Я]*', text)]
+
+    text = []
+    for el in vec_norm_words:
+        if el not in stop_words:
+            text.append(el)
+    return text
