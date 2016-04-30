a1dfeb08720df81e7f1023c5210aa3dc2b7a1670
Daniil_ef <dan.997@mail.ru>
2016-04-30 20:40:33 +0300

    Используется разреженная матрица. Для логов добавлен модуль logging

--- preprocessing_classification.py
+++ preprocessing_classification.py
@@ -1,40 +1,40 @@
 import json
+import logging
 import time
 
 import nltk
 import numpy
+import scipy
 from sklearn.cross_validation import LeaveOneOut
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.naive_bayes import GaussianNB
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.preprocessing import MaxAbsScaler
 
 from additional_features import add_feat
 
+logging.basicConfig(filename='db/log', level=logging.INFO
+                , filemode='a', format='%(asctime)s %(message)s'
+                , datefmt='%d/%m/%Y %H:%M:%S')
+
+
+with open('db/stop_word_dic', 'r') as f:
+    stop_words = f.read().split('\n')
+
+np = numpy
+classifier = MultinomialNB()
+tokenizer = nltk.tokenize.TweetTokenizer
+scaler = MaxAbsScaler()
+vectorizer = CountVectorizer(ngram_range=(1, 2),
+                             tokenizer=tokenizer().tokenize,
+                             stop_words=stop_words)
+
+fold_nums = 5
+loo = LeaveOneOut(fold_nums)
+mes_amount = -1  # Количество комментариев для каждого fold'а (-1 == все)
 
 def init():
-    with open('db/stop_word_dic', 'r') as f:
-        stop_words = f.read().split('\n')
 
-    np = numpy
-    classifier = GaussianNB()
-    tokenizer = nltk.tokenize.TweetTokenizer
-    scaler = MinMaxScaler((0, 2))
-    vectorizer = CountVectorizer(ngram_range=(1, 2),
-                                 tokenizer=tokenizer().tokenize,
-                                 stop_words=stop_words)
-
-    fold_nums = 5
-    loo = LeaveOneOut(fold_nums)
-    mes_amount = -1  # Количество комментариев для каждого fold'а (-1 == все)
-
-    log = ''
-    log += time.ctime(time.time())
-    log += '\nClassifier: ' + str(classifier)
-    log += '\nVectorizer: ' + str(vectorizer)
-    log += '\nTokenizer: ' + str(tokenizer)
-    log += '\nScaler: ' + str(scaler)
-    log += '\n'
-
+    # Слияние массивов
     def ravel(mas):
         merged = []
         for l in mas:
@@ -42,24 +42,24 @@ def init():
         return merged
 
     def fill_cross_val(key, fold_nums, mes_amount=500):
-        global log
         with open('db/people_sample', 'r') as f:
             people_sample = json.load(f)
-        log += ('Comments of each class: ' +
-            str(mes_amount if mes_amount != -1 else 'all') + '\n')
+
 
         messages = [[] for el in range(fold_nums)]
         mes_class = [[] for el in range(fold_nums)]
 
         for mas in people_sample[key]:
             mes_amount = mes_amount if mes_amount != -1 else len(mas)
-            submas = dict(list(mas.items())[:mes_amount])
+            mas = np.array(list(mas.items()))
+            np.random.shuffle(mas)
+            submas = dict(mas[:mes_amount])
             fold_len = len(submas) / fold_nums
-        i = 0
-        for key in submas.keys():
-            messages[int(i // fold_len)].append(key)
-            mes_class[int(i // fold_len)].append(submas[key])
-            i += 1
+            i = 0
+            for key in submas.keys():
+                messages[int(i // fold_len)].append(key)
+                mes_class[int(i // fold_len)].append(submas[key])
+                i += 1
 
         return [np.array(messages), np.array(mes_class)]
 
@@ -84,13 +84,13 @@ def init():
         #     Приведение к нормальному виду
 
         # Обработка обучающей выборки:
-        Com_train = vectorizer.fit_transform(Com_train).toarray()
-        Com_train = np.concatenate((Com_train, add_feat_train), axis=1)
+        Com_train = vectorizer.fit_transform(Com_train)
+        Com_train = scipy.sparse.hstack([Com_train, add_feat_train])
         Com_train = scaler.fit_transform(Com_train)
 
         # Обработка тестирующей выборки
-        Com_test = vectorizer.transform(Com_test).toarray()
-        Com_test = np.concatenate((Com_test, add_feat_test), axis=1)
+        Com_test = vectorizer.transform(Com_test)
+        Com_test = scipy.sparse.hstack([Com_test, add_feat_test])
         Com_test = scaler.transform(Com_test)
 
 
@@ -109,5 +109,16 @@ def init():
     print(str((estim / fold_nums) * 100) + ' %')
 
     # Запись логов в файл
-    with open('db/log', 'a') as f:
-        f.write(log + str((estim / fold_nums) * 100) + ' %\n\n')
+
+    logging.info(time.ctime(time.time())
+             + '\nClassifier: ' + str(classifier)
+             + '\nVectorizer: ' + str(vectorizer)
+             + '\nTokenizer: ' + str(tokenizer)
+             + '\nScaler: ' + str(scaler)
+             + 'shufle: on'
+             + 'Comments of each class: '
+             + str(mes_amount if mes_amount != -1 else 'all')
+             + "Estimation" + str((estim / fold_nums) * 100) + ' %\n\n'
+             )
+
+init()
